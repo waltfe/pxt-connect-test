54e054cf0d0c7883efd4e021de33639cbf2cf06e
鄢梦阳 <510857289@qq.com>
2024-08-01 10:57:00 +0800

    no msg

--- main.ts
+++ main.ts
@@ -1,1 +1,15 @@
 
+
+/**
+* Functions to ConnectTest by ELECFREAKS Co.,Ltd.
+*/
+//% color=#00B1ED  icon="\uf005" block="ConnectTest" blockId="ConnectTest"
+namespace ConnectTest {
+
+    //% blockId="conntest" block="conntest"
+    //% shim=ble::main
+    //% parts=ble trackArgs=0
+    export function test() : number {
+        return 32.6;
+    }
+}
